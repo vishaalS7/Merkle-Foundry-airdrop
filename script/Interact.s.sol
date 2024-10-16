a4251c0aaeb6ec924e631b589f65c980d0293ccb
vishaalS7 <ivishaal1411@gmail.com>
2024-10-16 16:50:09 +0530

    chore: forge init

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import { Script, console } from "forge-std/Script.sol";
-import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
+import { DevOpsTools } from "../lib/foundry-devops/src/DevOpsTools.sol";
 import { MerkleAirdrop } from "../src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
